10daeb060e61cc701d7879904e0a3c6fc4211f92
Lima <jonathan.da.s.lima@accenture.com>
2020-06-08 00:00:46 -0300

    add middleware express.json and serving static files via uploads route

--- nlw1/aulas/server/src/server.ts
+++ nlw1/aulas/server/src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
-import routes from './routes';
+import express from "express";
+import routes from "./routes";
+import path from "path";
 
 const app = express();
+app.use(express.json())
 
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
